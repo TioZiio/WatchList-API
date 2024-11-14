de6877774a0cdb533014944260ef5ddfc84debf3
TioZiio <davidmaf@gmail.com>
2024-11-14 16:45:21 -0300

    Adicionando Html e CSS na página inicial

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
 from .models import MediaItens
 from .serializers import MediaItensSerializer
+from django.shortcuts import render
 
 class MediaItensViewSet(viewsets.ModelViewSet):
 
     queryset = MediaItens.objects.all()
     serializer_class = MediaItensSerializer
 
+def index(request):
+    return render(
+        request,
+        'global/index.html',
+    )
--- watchlist/urls.py
+++ watchlist/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapp.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls, name='admin'),
     path('api/', include('myapp.urls'), name='api')
 ]
