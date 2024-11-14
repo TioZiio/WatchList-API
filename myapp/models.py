70b6a6c1d10fda6711876ba31bfcb1b6b751c2b2
TioZiio <davidmaf@gmail.com>
2024-11-14 11:01:53 -0300

    Criando model MediaItens

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class MediaItens(models.Model):
+    MEDIA_TYPES = [
+        ('movie','Movie'),
+        ('series','Series'),
+        ('anime', 'Anime'),
+    ]
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=15, choices=MEDIA_TYPES)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    register = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__() -> str:
+        return self.name
+    
